2632bc359b02a59a9f701e354dc5e06c75bb7bc5
Safat Mobid <safatmobid@gmail.com>
2022-11-03 10:37:37 -0400

    W.I.P

--- scripts/index.js
+++ scripts/index.js
@@ -10,21 +10,48 @@ window.onload = function () {
     teamslistDropdown();
     const infoBtn = document.getElementById("teaminfoBtn");
     infoBtn.onclick = displayInfo;
+    teams.value = "TX";
 }
 
 
 function teamslistDropdown()
 {
     const teamList = document.getElementById("teamList")
+    
     for (let i = 0; i < teams.length; i++) 
     {
     let tName = teams[i].name
     let tCode = teams[i].code
     let tPlays = teams[i].plays
 
-    let option = new Option (tName, tCode);
+    
+    
+    let option = new Option (tName, tCode); //+ tCode); can add this to bring both name and code to menu 
 
     teamList.appendChild(option);
+    }
+}
+
+function displayInfo()
+{   
+    let teamInfo = document.getElementById("teamInfo")
+    
+    for (let i = 0; i < teams.length; i++) 
+    {
+    let tName = teams[i].name
+    let tCode = teams[i].code
+    let tPlays = teams[i].plays
+    let blankTeam = ""
+
+    if (teamList.value == teamList[i].value) 
+    {
+        teamInfo.innerHTML = "You selected " + tName + "( " + tCode + " )" +  " who plays in " + tPlays
 
     }
+    else if (teamList.value == blankTeam)
+    {
+        teamInfo.innerHTML = "Please pick a team"
+    }
+}
+return false;
 }
